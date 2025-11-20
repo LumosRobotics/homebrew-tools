0d33b86a9695abc34dcf6598c9143c47b762d80f
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2025-11-20 20:54:50 +0100

    Updated formula

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -1,21 +1,12 @@
-class Lumos < Formula
-  desc "Lumos command-line tool"
-  homepage "https://github.com/LumosRobotics/LumosTool"
-  url "https://github.com/LumosRobotics/LumosTool/releases/download/v1.0.0/lumos-macos-1.0.0.tar.gz"
+cask "lumos" do
+  version "1.0.0"
   sha256 "df78b50274605adc444710f6f356adc4e789868540e955785352cfd6c4712681"
-  license "MIT"
 
-  def install
-    # If it contains multiple files or folders, specify the correct path(s).
-    # Example:
-    bin.install "bin/lumos"
+  url "https://github.com/LumosRobotics/LumosTool/releases/download/v1.0.0/lumos-macos-1.0.0.tar.gz"
+  name "Lumos"
+  desc "Lumos Robotics CLI tooling"
+  homepage "https://github.com/LumosRobotics/LumosTool"
 
-    share.install Dir["share/*"]
-  end
-
-  test do
-    # Check that it at least runs and prints a version or help
-    output = shell_output("#{bin}/lumos --version")
-    assert_match "1.0.0", output
-  end
+  # Where the binary lives inside the extracted archive
+  binary "bin/lumos"
 end
