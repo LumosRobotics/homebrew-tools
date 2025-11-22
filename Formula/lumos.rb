eda6e5a36304cf43d2a2b5b7effdd2137783c954
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2025-11-22 18:19:56 +0100

    Removed bottle

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -7,9 +7,6 @@ class Lumos < Formula
   sha256 "df78b50274605adc444710f6f356adc4e789868540e955785352cfd6c4712681"
   license "MIT"
 
-  # This tells Homebrew not to treat it as source needing a build
-  bottle :unneeded
-
   def install
     # Install CLI entrypoint
     bin.install "bin/lumos"
